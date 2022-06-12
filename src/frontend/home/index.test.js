16606ec4d279ecd0cfee50c0583609d048594151
Bressain Dinkelman <dinkelburt@gmail.com>
2022-06-12 15:46:04 -0600

    Fix broken test

--- src/frontend/home/index.test.js
+++ src/frontend/home/index.test.js
@@ -1,11 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom'
 import { TestQueryClientProviderWrapper } from '../util/tests';
 import Home from './index';
 
 test('renders home page', async () => {
   render(
     <TestQueryClientProviderWrapper>
-      <Home />
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
     </TestQueryClientProviderWrapper>
   );
   const homeElement = screen.getByText(/spice/i);
